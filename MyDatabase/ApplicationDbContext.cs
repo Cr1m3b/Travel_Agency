56c4ae4740f9c5786d8bb3b02689c93320a0c7bc
Cr1m3b <iosifvasoul@yahoo.gr>
2022-02-27 16:42:34 +0100

    Add project files.

---
+++ MyDatabase/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Entities.IdentityUsers;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDatabase.Initializers;
+
+namespace MyDatabase
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false) 
+        {
+            Database.SetInitializer<ApplicationDbContext>(new MockupDbInitializer());
+            Database.Initialize(false);
+        }
+        
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        //Declaration of the DbSets
+        
+    }
+}
---
+++ MyDatabase/Seeding/SeedingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDatabase.Seeding
+{
+    public class SeedingService
+    {
+        //Dependency Injection
+        ApplicationDbContext db;
+        public SeedingService(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        //Implementation of Seeding Methods ex. public void SeedPackages()
+
+    }
+}
