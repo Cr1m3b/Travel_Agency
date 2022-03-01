ebf22cec2584ac620403e75388988bb7cfe794a3
vicky kalantzi <vickykalantzi@hotmail.gr>
2022-03-01 14:11:12 +0100

    HotelController

---
+++ Travel_Agency/Controllers/HotelController.cs
@@ -0,0 +1,100 @@
+﻿using MyDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Travel_Agency.Controllers
+{
+    public class HotelController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Hotel
+        public ActionResult Index()
+        {
+            //var hotels=db.Hotels.Include(h=>h.);   
+            //return View();
+        }
+
+        // GET: Hotel/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Hotel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hotel/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hotel/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Hotel/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Hotel/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Hotel/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
