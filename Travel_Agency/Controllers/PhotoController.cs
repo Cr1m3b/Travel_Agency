752a773eadf131ec457d4707966b197146685f20
vicky kalantzi <vickykalantzi@hotmail.gr>
2022-03-06 09:03:18 +0100

    Rating and Photo Controllers

---
+++ Travel_Agency/Controllers/PhotoController.cs
@@ -0,0 +1,31 @@
+﻿using MyDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Travel_Agency.Controllers
+{
+    public class PhotoController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Photo
+        public ActionResult Index()
+        {
+            var photos=db.Photos;
+            return View(photos);
+        }
+
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
