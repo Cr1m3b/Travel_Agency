8c75f7233e6700a99979ef65081987db2de023ec
vicky kalantzi <vickykalantzi@hotmail.gr>
2022-03-18 16:34:00 +0100

    Home offers

---
+++ PersistenceLayer/Repositories/HomeRepository.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Models;
+using MyDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistenceLayer.Repositories
+{
+    public class HomeRepository
+    {
+        private ApplicationDbContext db;
+
+       
+
+        public HomeRepository(ApplicationDbContext MyContent)
+        {
+            db = MyContent; 
+        }
+         
+        public IEnumerable<Package> GetThreeMaxDiscounts()
+        {
+
+         var packages= db.Packages.Where(p=>p.Discount!=0).OrderBy(p=>p.Discount).Take<Package>(3).ToList();
+
+
+
+            return packages; 
+
+
+
+
+
+
+        }
+        
+
+    }
+}
