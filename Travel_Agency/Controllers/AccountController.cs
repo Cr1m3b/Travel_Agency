b74599485e61c0b2a93fdb9d4f6c86afce8714c5
Cr1m3b <iosifvasoul@yahoo.gr>
2022-04-07 21:18:52 +0200

    Home fix

--- Travel_Agency/Controllers/AccountController.cs
+++ Travel_Agency/Controllers/AccountController.cs
@@ -78,7 +78,6 @@ namespace Travel_Agency.Controllers
             {
                 return View(model);
             }
-
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
--- Travel_Agency/Controllers/AdminControllers/AdminBookingController.cs
+++ Travel_Agency/Controllers/AdminControllers/AdminBookingController.cs
@@ -26,7 +26,7 @@ namespace Travel_Agency.Controllers.AdminController
         public ActionResult Index(string sortOrder)
         {
             //  var bookings = db.Bookings.Include(b => b.ApplicationUser).Include(b => b.Packages).ToList();
-            var bookings = repository.GetAllWithRelatedTables().OrderByDescending(b => b.PurchaseDate);
+            var bookings = repository.GetAllWithRelatedTables();
             switch (sortOrder)
             {
                 case "BookAscend": bookings = bookings.OrderBy(b => b.PurchaseDate).ToList();break;
