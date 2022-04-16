dee998305cd03dd762c13885a87509b5a1eb0c73
Cr1m3b <iosifvasoul@yahoo.gr>
2022-04-16 18:46:47 +0200

    Client User Edit

--- Travel_Agency/Models/ManageViewModels.cs
+++ Travel_Agency/Models/ManageViewModels.cs
@@ -7,6 +7,7 @@ namespace Travel_Agency.Models
 {
     public class IndexViewModel
     {
+        public string UserName { get; set; }
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
